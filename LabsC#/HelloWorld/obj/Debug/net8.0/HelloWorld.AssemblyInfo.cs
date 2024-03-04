b6ea72c708e07d09097dd225267076080551d5ea
Rafael Acevedo <racevedoramos35@gmail.com>
2024-03-04 09:40:39 -0400

    bruh

--- LabsC#/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ LabsC#/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+526bd3aabd71070c3c1a1ab213a07aa6d67633d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3114094d437b18bc767b1df88cfcd8ece95a1b12")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
