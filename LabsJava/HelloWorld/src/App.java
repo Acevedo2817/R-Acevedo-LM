73d85066013b1774c0e26d312d2e45be397fc76b
Rafael Acevedo <racevedoramos35@gmail.com>
2024-03-10 17:03:19 -0400

    ...

--- LabsJava/HelloWorld/src/App.java
+++ LabsJava/HelloWorld/src/App.java
@@ -1,5 +1,7 @@
+// This is worthless.
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World!");
     }
 }
