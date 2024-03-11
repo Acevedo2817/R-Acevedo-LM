09873b2ef703b2a360050d45cc89804e970f6dbe
Rafael Acevedo <racevedoramos35@gmail.com>
2024-03-11 08:43:56 -0400

    ...

--- LabsC#/test-app/obj/Debug/net8.0/app.AssemblyInfo.cs
+++ LabsC#/test-app/obj/Debug/net8.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d85066013b1774c0e26d312d2e45be397fc76b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ab18522835fa753447c485392f2a39040bfe660")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
