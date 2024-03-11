48076dcde31578b355dbd441157fc05ead76f014
Rafael Acevedo <racevedoramos35@gmail.com>
2024-03-11 09:27:55 -0400

    ...

---
+++ LabsC#/work/test1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program{
+    static void Main(){
+        Console.Write("Type in a number. ");
+        double number1 = int.Parse(Console.ReadLine());
+        const int three = 3;
+        double result = number1 / three;
+        Console.WriteLine("Result: " + result);
+    }
+}
